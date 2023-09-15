fd659fffa80f40d3fe24381928f9c6073420fe30
Robin Littiere <zauts95@gmail.com>
2023-09-15 15:12:00 +0200

    success route and logic, env path also

--- controllers/devices.go
+++ controllers/devices.go
@@ -13,6 +13,7 @@ func AdminRoutes(r *gin.RouterGroup) {
 	{
 		deviceGroup.GET("/", GetDevices)
 		deviceGroup.GET("/errors", GetErrorUrls)
+		deviceGroup.GET("/success", GetSuccessUrls)
 
 	}
 }
@@ -35,6 +36,21 @@ func GetDevices(c *gin.Context) {
 func GetErrorUrls(c *gin.Context) {
 	// Here parse data from file
 	datapath := os.Getenv("ERROR_FILE")
+	data, err := services.ParseTxtFile(datapath)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"data": data,
+	})
+}
+
+func GetSuccessUrls(c *gin.Context) {
+	// Here parse data from file
+	datapath := os.Getenv("SUCCESS_FILE")
 	data, err := services.ParseTxtFile(datapath)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
